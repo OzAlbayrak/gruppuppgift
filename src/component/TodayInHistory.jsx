73e33133a6485f3e33c7c682089e00af47609574
OzAlbayrak <oz@albayrak.dev>
2023-01-17 15:48:42 +0100

    added all functionalityes

--- src/component/TodayInHistory.jsx
+++ src/component/TodayInHistory.jsx
@@ -1,51 +1,58 @@
-import React, { useState } from "react";
-import useFetch from "./UseFetch";
+import React, { useState, useEffect} from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 
 const TodayInHistory = () => {
-  //var dateMonth = 2;
-  //var dateDay = 14;
   const [dateMonth, setDateMonth] = useState("1");
   const [dateDay, setDateDay] = useState("1");
-  const { data, isPending, error } = useFetch(
-    `http://history.muffinlabs.com/date/${dateMonth}/${dateDay}`
-  );
-  const monthArray = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
+  const[data, setData] = useState(null);
+  const[isPending, setIsPending] = useState(true);
+  const[error, setError] = useState(null);
+  const[randomEvent, setRandomEvent] = useState(null);
+  const[randomBirth, setRandomBirth] = useState(null);
+  const[randomDeath, setRandomDeath] = useState(null);
+  const monthArray = [1,2,3,4,5,6,7,8,9,10,11,12];
   const dayArray = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
     22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
   ];
-  var event = [];
-  var death = [];
-  var birth = [];
-  var text = "";
-  var year = "";
+  var address ="";
+  var month = require('month');
+
+  useEffect(() => {
+    }, []);
 
   const monthOnChangeHandler = (e) => {
     setDateMonth(e.target.value);
-    //const { data, isPending, error } = useFetch("http://history.muffinlabs.com/date/${dateMonth}/${dateDay}");
   };
 
   const dayOnChangeHandler = (e) => {
     setDateDay(e.target.value);
-    //const { data, isPending, error } = useFetch("http://history.muffinlabs.com/date/2/14");
   };
 
-  const getNewData = () => {};
+  const getNewData = e => {
+    setIsPending(true);
+    address = 'http://history.muffinlabs.com/date/' + dateMonth + '/' + dateDay;
+    fetch(address)
+      .then(res => {
+          if(!res.ok) {
+              throw Error('Kunde ej hämta data');
+          }
+          return res.json();
+      })
+      .then(data => {
+          setData(data);
+          setIsPending(false);
+          setError(null);
+          setRandomEvent(Math.floor(Math.random() * data.data.Events.length));
+          setRandomBirth(Math.floor(Math.random() * data.data.Births.length));
+          setRandomDeath(Math.floor(Math.random() * data.data.Deaths.length));
+      })
+      .catch(err => {
+          setIsPending(false);
+          setError(err.message);
+      })
+    };
 
   return (
     <div>
@@ -53,12 +60,13 @@ const TodayInHistory = () => {
       <h2>Choose a date</h2>
       <select onChange={monthOnChangeHandler}>
         {monthArray.map((x) => (
-          <option value={x}>{x}</option>
+          <option value={x}>{month(x)}</option>
         ))}
       </select>
 
       <select onChange={dayOnChangeHandler}>
-        {dayArray.map((x) => (
+        {
+        dayArray.map((x) => (
           <option value={x}>{x}</option>
         ))}
       </select>
@@ -66,7 +74,6 @@ const TodayInHistory = () => {
       <button className="get-new-button" onClick={getNewData}>
         New Event
       </button>
-      <button className="clear-button">Clear Event</button>
 
       <Tabs>
         <TabList>
@@ -75,24 +82,40 @@ const TodayInHistory = () => {
           <Tab>Death</Tab>
         </TabList>
 
-        <TabPanel>
-          
+        <TabPanel> 
         <div>{console.log(data)}</div>
           {
             isPending ? (<>
               <div>Loading...</div>
             </>) : (<>
-              <div>År: {data.data.Events[0].year}</div>
-              <div>Event of the day: {data.data.Events[0].text}</div>
+              <div>Year: {data.data.Events[randomEvent].year}</div>
+              <div>Event of the day: {data.data.Events[randomEvent].text}</div>
             </>)
           }
+        </TabPanel>
 
-        </TabPanel>
         <TabPanel>
-          <h2>data.birth.year data.birth.text</h2>
+        <div>{console.log(data)}</div>
+          {
+            isPending ? (<>
+              <div>Loading...</div>
+            </>) : (<>
+              <div>Year: {data.data.Births[randomBirth].year}</div>
+              <div>Birth of the day: {data.data.Births[randomBirth].text}</div>
+            </>)
+          }
         </TabPanel>
+
         <TabPanel>
-          <h2>data.death.year data.death.text</h2>
+        <div>{console.log(data)}</div>
+          {
+            isPending ? (<>
+              <div>Loading...</div>
+            </>) : (<>
+              <div>Year: {data.data.Deaths[randomDeath].year}</div>
+              <div>Death of the day: {data.data.Deaths[randomDeath].text}</div>
+            </>)
+          }
         </TabPanel>
       </Tabs>
     </div>
@@ -100,39 +123,3 @@ const TodayInHistory = () => {
 };
 
 export default TodayInHistory;
-
-/*
-
- {data.Events[0].text}
-
-
-
-<select onChange={monthOnChangeHandler}>
-        <option value="1">1</option>
-        <option value="12">12</option>
-      </select>
-
-      <select onChange={dayOnChangeHandler}>
-        <option value="1">1</option>
-        <option value="31">12</option>
-      </select>
-
-
-
-
-
-
-
-<>
-          {data.Events.map((item, index) => (
-              <div key={index}>
-                <div>year: {item.year}</div>
-                <div>text: {item.text}</div>
-              </div>
-            ))}
-        </>
-
-
-
-
-*/
